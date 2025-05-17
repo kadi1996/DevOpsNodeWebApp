f0221c55a5f95fc56248d8f0d7ceadb3f55c539e
Kadircan Güney <gueneykadircan@gmail.com>
2025-05-18 00:53:40 +0200

    last fixes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! Now Docker Deployment with GitHub Actions!!! Es ist Samstag Abend :)))');
+  res.send('Hello FS2025 DevOps Course! Now Docker Deployment with GitHub Actions!!! Es ist Samstag Abend :))) und ich bin bald fertig. Cloud Deploy auf Render.com');
 });
 
 app.listen(PORT, HOST);
