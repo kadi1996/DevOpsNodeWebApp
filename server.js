d80f0c6c9e520b4958e785a7789a0e2fd45929a1
Kadircan Güney <gueneykadircan@gmail.com>
2025-05-17 23:15:12 +0200

    test commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! Now Docker Deployment with GitHub Actions!!!');
+  res.send('Hello FS2025 DevOps Course! Now Docker Deployment with GitHub Actions!!! Es ist Samstag Abend :)))');
 });
 
 app.listen(PORT, HOST);
